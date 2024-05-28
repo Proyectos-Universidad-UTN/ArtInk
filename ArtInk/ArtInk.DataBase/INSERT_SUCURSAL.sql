1bf1c57709f23a9a4f89f2fab325b7f4c58089f9
Brayanvangarden <gerardo16@hotmail.com.ar>
2024-05-27 23:05:29 -0600

    Cambios en la bd

---
+++ ArtInk/ArtInk.DataBase/INSERT_SUCURSAL.sql
@@ -0,0 +1,31 @@
+﻿USE Artink 
+GO
+
+SET IDENTITY_INSERT Sucursal ON;
+
+IF NOT EXISTS (SELECT 1 FROM Sucursal WHERE Id = 1)
+BEGIN 
+	INSERT INTO Sucursal (
+       [Id]
+      ,[Nombre]
+      ,[Descripcion]
+      ,[Telefono]
+      ,[CorreoElectronico]
+      ,[IdDistrito]
+      ,[DireccionExacta]
+      ,[FechaCreacion]
+      ,[UsuarioCreacion])
+    VALUES (
+        1,
+        'Jack Piercing',
+        'Tatuajes y piercing',
+        24305256,
+        'jackpiercing@gmail.com',
+        124,
+        'Detrás del museo de Juan Santamaría',
+        GETDATE (),
+        'ArtInk'
+    )
+END
+
+SET IDENTITY_INSERT Sucursal OFF;
