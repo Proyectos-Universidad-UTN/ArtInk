3b79c153043f9137cbcfd101a09badce1e525a04
Brayanvangarden <gerardo16@hotmail.com.ar>
2024-06-09 19:19:43 -0600

    Corregir css y js

--- ArtInk/ArtInk.Site/wwwroot/js/site.js
+++ ArtInk/ArtInk.Site/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//************************DETALLEPRODUCTO************************* */
+var script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js';
+document.head.appendChild(script);
+
+//************************ENDDETALLEPRODUCTO************************* */
