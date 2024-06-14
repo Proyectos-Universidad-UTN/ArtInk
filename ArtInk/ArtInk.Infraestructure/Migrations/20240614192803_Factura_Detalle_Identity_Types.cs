e2f4b3b00906f4acdd85a85b776232cab69fbaeb
Brayanvangarden <gerardo16@hotmail.com.ar>
2024-06-14 16:16:47 -0600

    arreglos en la migracion

---
+++ ArtInk/ArtInk.Infraestructure/Migrations/20240614192803_Factura_Detalle_Identity_Types.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtInk.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Factura_Detalle_Identity_Types : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Factura",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "IdDetalleFactura",
+                table: "DetalleFacturaProducto",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "DetalleFacturaProducto",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "IdFactura",
+                table: "DetalleFactura",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "DetalleFactura",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<short>(
+                name: "Id",
+                table: "Factura",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<short>(
+                name: "IdDetalleFactura",
+                table: "DetalleFacturaProducto",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<short>(
+                name: "Id",
+                table: "DetalleFacturaProducto",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AlterColumn<short>(
+                name: "IdFactura",
+                table: "DetalleFactura",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<short>(
+                name: "Id",
+                table: "DetalleFactura",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
