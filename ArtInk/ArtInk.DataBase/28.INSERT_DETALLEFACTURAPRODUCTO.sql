489f39995b025852389f914a25b6768ec2deb308
Brayanvangarden <gerardo16@hotmail.com.ar>
2024-06-15 10:41:13 -0600

    Factura, detallefactura, detallefacturaproducto

--- ArtInk/ArtInk.DataBase/28.INSERT_DETALLEFACTURAPRODUCTO.sql
+++ ArtInk/ArtInk.DataBase/28.INSERT_DETALLEFACTURAPRODUCTO.sql
@@ -10,6 +10,10 @@ BEGIN
 	([Id],[IdDetalleFactura],[IdProducto],[Cantidad])
 	VALUES
 	 (1,1,1,1.00)
+	,(2,1,1,5.00)
+	,(3,2,2,3.00)
+	,(4,3,3,4.00)
+	,(5,4,4,2.00)
 	SET IDENTITY_INSERT dbo.DetalleFacturaProducto OFF;
 	
 END
