c56dcc87cf857c794513e26f895b7e16f0a4b73d
David <davidsoto_2409@outlook.com>
2024-06-19 20:17:44 -0600

    inserts

---
+++ ArtInk/ArtInk.DataBase/27.INSERT_DETALLEFACTURA.sql
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtInk.DataBase
+{
+    class _27
+    {
+    }
+}
