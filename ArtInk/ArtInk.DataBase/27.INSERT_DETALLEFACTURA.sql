0d918deac882e2fcc7e6104e044b0961b859a164
David <davidsoto_2409@outlook.com>
2024-06-19 20:18:18 -0600

    Insert

--- ArtInk/ArtInk.DataBase/27.INSERT_DETALLEFACTURA.sql
+++ ArtInk/ArtInk.DataBase/27.INSERT_DETALLEFACTURA.sql
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿USE Artink 
+GO
 
-namespace ArtInk.DataBase
-{
-    class _27
-    {
-    }
-}
+IF NOT EXISTS(SELECT 1 FROM DetalleFactura WHERE Id = 1)
+BEGIN
+	
+	SET IDENTITY_INSERT dbo.DetalleFactura ON
+
+	INSERT INTO dbo.DetalleFactura
+	([Id],[IdFactura],[IdServicio],[NumeroLinea],[Cantidad],[TarifaServicio],[MontoSubtotal],[MontoImpuesto],[MontoTotal])
+	VALUES
+	 (1,1,1,1,2,50.0000,100.0000,13.0000,113.0000)
+	SET IDENTITY_INSERT dbo.DetalleFactura OFF;
+END
