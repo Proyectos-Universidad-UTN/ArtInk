0db3193504efb4a903598e02519ac9ebb02ecf67
David <davidsoto_2409@outlook.com>
2024-07-01 13:26:45 -0600

    REQUIRES

--- ArtInk/ArtInk.Site/ViewModels/Request/ServicioRequestDTO.cs
+++ ArtInk/ArtInk.Site/ViewModels/Request/ServicioRequestDTO.cs
@@ -1,4 +1,6 @@
 ﻿using ArtInk.Site.ViewModels.Response;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ArtInk.Site.ViewModels.Request
 {
@@ -6,14 +8,22 @@ namespace ArtInk.Site.ViewModels.Request
     {
         public byte Id { get; set; }
 
+        [DisplayName("Servicio")]
+        [Required(ErrorMessage = "El servicio es requerido")]
         public string Nombre { get; set; } = null!;
 
+        [DisplayName("Descripción")]
+        [Required(ErrorMessage = "La descripción es requerida")]
+        [MaxLength(150)]
         public string Descripcion { get; set; } = null!;
 
+        [DisplayName("Tipo de servicio")]
         public byte IdTipoServicio { get; set; }
 
+        [Required(ErrorMessage = "La tarifa es requerida")]
         public decimal Tarifa { get; set; }
 
+        [DisplayName("Observación")]
         public string? Observacion { get; set; }
 
         public bool Activo { get; set; }
