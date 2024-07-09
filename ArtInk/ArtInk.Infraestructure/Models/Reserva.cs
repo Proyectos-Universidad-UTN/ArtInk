ac683959edf0d0bcdc3614732d6ab3a993190ff6
dasot <145491385+dasot@users.noreply.github.com>
2024-07-08 18:30:39 -0600

    Inserts factura (#10)
    * Cambios  en usuario
    
    * -Cambiar el Home
    -Modificar producto, rol y usuario
    
    * Editar home, producto , rol y usuario
    
    * Modificar detalles
    
    * DetalleFacturaProducto, factura, DetalleFactura
    
    * Update order of executions
    
    * Factura, detallefactura, detallefacturaproducto
    
    * SQL
    
    * Ajuste orden inserts
    
    ---------
    
    Co-authored-by: Brayanvangarden <gerardo16@hotmail.com.ar>
    Co-authored-by: alejograjal <alejandro.grajal.s@gmail.com>

--- ArtInk/ArtInk.Infraestructure/Models/Reserva.cs
+++ ArtInk/ArtInk.Infraestructure/Models/Reserva.cs
@@ -24,6 +24,7 @@ public partial class Reserva
     public DateTime? FechaModificacion { get; set; }
 
     public string? UsuarioModificacion { get; set; }
+    
     public short IdUsuarioSucursal { get; set; }
 
     public virtual SucursalHorario IdSucursalHorarioNavigation { get; set; } = null!;
