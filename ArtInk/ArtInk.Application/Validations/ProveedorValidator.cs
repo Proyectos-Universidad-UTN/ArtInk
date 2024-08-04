ecb68c4e83c7e39b845a457a5dad1260d30cdc2e
alejograjal <alejandro.grajal.s@gmail.com>
2024-08-04 17:21:27 -0600

    Agregar validación para proveedor

--- ArtInk/ArtInk.Application/Validations/ProveedorValidator.cs
+++ ArtInk/ArtInk.Application/Validations/ProveedorValidator.cs
@@ -7,6 +7,30 @@ public class ProveedorValidator : AbstractValidator<Proveedor>
 {
     public ProveedorValidator()
     {
-        
+        RuleFor(m => m.Nombre)
+            .NotNull().WithMessage("Nombre requerido")
+            .NotEmpty().WithMessage("Debe especificar el nombre");
+
+        RuleFor(m => m.CedulaJuridica)
+            .NotNull().WithMessage("Cédula jurídica requerido")
+            .NotEmpty().WithMessage("Debe especificar la Cédula jurídica");
+
+        RuleFor(m => m.RasonSocial)
+            .NotNull().WithMessage("Rason social requerido")
+            .NotEmpty().WithMessage("Debe especificar la rason social");
+
+        RuleFor(x => x.Telefono)
+            .NotEmpty().WithMessage("Por favor ingrese el teléfono");
+
+        RuleFor(x => x.CorreoElectronico)
+            .NotEmpty().WithMessage("Por favor ingrese el correo electrónico")
+            .EmailAddress().WithMessage("Por favor ingrese una dirección de correo electrónico válido");
+
+        RuleFor(x => x.IdDistrito)
+            .NotEmpty().WithMessage("Por favor ingrese un distrito válido");
+
+        RuleFor(x => x.DireccionExacta)
+            .NotEmpty().WithMessage("Por favor ingrese un distrito válido")
+            .When(x => x.DireccionExacta != null);
     }
 }
