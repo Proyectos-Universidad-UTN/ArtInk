f62759047c69a8b32382040afbaebe157d560514
alejograjal <alejandro.grajal.s@gmail.com>
2024-08-14 17:59:00 -0600

    Adjusting duplicates

--- ArtInk/ArtInk.Site/ViewComponents/General/NavigationViewComponent.cs
+++ ArtInk/ArtInk.Site/ViewComponents/General/NavigationViewComponent.cs
@@ -1,28 +1,11 @@
 using System.Security.Claims;
+using ArtInk.Site.Common;
 using ArtInk.Utils;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ArtInk.Site.ViewComponents.General;
 
-public class NavigationViewComponent(IHttpContextAccessor httpContextAccessor) : ViewComponent
+public class NavigationViewComponent(ICurrentUserAccessor currentUserAccessor) : ViewComponent
 {
-    public IViewComponentResult Invoke()
-    {
-        var jwtCookie = httpContextAccessor.HttpContext!.Request.Cookies["JWT"];
-        if (jwtCookie == null) return View(new UsuarioJWT() { Role = string.Empty });
-
-        var (keyId, _auience, claims) = JwtToken.DecodeJwt(jwtCookie);
-
-        var usuarioJWT = new UsuarioJWT()
-        {
-            IdUsuario = short.Parse(claims.Single(m => m.Type == "IdUsuario").Value),
-            Nombre = claims.Single(m => m.Type == "Nombre").Value,
-            Apellidos = claims.Single(m => m.Type == "Apellidos").Value,
-            NombreCompleto = claims.Single(m => m.Type == "NombreCompleto").Value,
-            CorreoElectronico = claims.Single(m => m.Type == "CorreoElectronico").Value,
-            Role = claims.Single(m => m.Type == "role").Value,
-        };
-
-        return View(usuarioJWT);
-    }
+    public IViewComponentResult Invoke() => View(currentUserAccessor.GetCurrentUser());
 }
